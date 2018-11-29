5ceed8f5c0e51c877268fe134429bf8e77c218b4
NoelleDL <noelle.legrain@gmail.com>
2018-11-29 16:21:32 +0000

    Second test and method returning a words for numbers twenty and below

--- spec/NumbersInWordsSpec.js
+++ spec/NumbersInWordsSpec.js
@@ -3,4 +3,9 @@ describe("Converter", function () {
     result = Converter(1);
     expect(result).toEqual("one");
   });
+
+  it("should return 'eleven' when given 1", function() {
+    result = Converter(11);
+    expect(result).toEqual("eleven");
+  });
 });
